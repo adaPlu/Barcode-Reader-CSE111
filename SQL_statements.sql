1dd6b3c9663957d3ae2e4c84c6bde2f1089e5582
csalasvalentin <61982606+csalasvalentin@users.noreply.github.com>
2020-11-08 14:30:07 -0800

    Add files via upload
    SQL statements that we can use to test our db

---
+++ SQL_statements.sql
@@ -0,0 +1,59 @@
+--Q1
+--Print all the stores with the total number of suppliers that they have
+
+--Q2
+--Print Country with the highest and lowest shiping rate
+
+--Q3
+--Print inventory that needs to restock
+
+--Q4
+--Print producer's city and country
+
+--Q5
+--Print supplier and producer with same shipping rate
+
+--Q6
+--Print suppliers with shipping rate <.15
+
+--Q7
+--Print store with their producers city
+
+--Q8
+--Print the store that received at least .11 shipping rate in more than 10 products
+
+--Q9
+-- Print the number of suppliers from every country
+
+--Q10
+--Print avg shipping rate for each country and sort in alphabetical order
+
+--Q11
+--Delete country with shipping rate <.09
+
+--Q12
+--Update Norway's shipping rate to .20
+
+--Q13
+--Delete country with mininum shipping rate
+
+--Q14
+--Print city with more than 2 different supplier
+
+--Q15
+--Print city with the most suppliers
+
+--Q16
+--Print city whos inventory is the lowest
+
+--Q17
+--UPDATE cities whos supplier has a shipping rate of <.10 to a rate of (current rate/2)*2.5
+
+--Q18
+--Whats the avg amount of stores in a city
+
+--Q19
+--Whats the store's shipping rate for customer CC360
+
+--Q20
+--Insert barcode 000111010 to inventory 
