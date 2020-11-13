698fd1ce1ac32a372372f4f9a5583590cd75e512
apluguez <apluguez@ucmerced.edu>
2020-11-13 10:40:11 -0800

    Moved all queries to queries.sql, updated text file data country, removed shipping rate.

--- db.sql
+++ db.sql
@@ -1,62 +1,63 @@
 ------------------------------TABLES CREATED------------------------------
 
-CREATE TABLE Inventory(
+CCREATE TABLE IF NOT EXISTS Inventory(
  i_storeID    VARCHAR(30) not NULL,
  i_barcode  INT(15,0) not NULL,
  i_stock       INT(15,0) not NULL
 );
 
-CREATE TABLE Producer(
+CREATE TABLE IF NOT EXISTS Producer(
  pr_name VARCHAR(25) not NULL,
  pr_type VARCHAR(25) not NULL,
  pr_cityKey  INT(15,0) not NULL,   
- pr_shippingRate  INT(15,0) not NULL
+ pr_shippingRateKey  INT(15,0) not NULL
 );
 
-CREATE TABLE Supplier(
+
+CREATE TABLE IF NOT EXISTS Supplier(
  s_supplierID  VARCHAR(25) not NULL,
  s_cityKey      INT(15,0) not NULL,
- s_shippingRate  INT(15,0) not NULL
+ s_shippingRateKey  INT(15,0) not NULL
 );
 
-CREATE TABLE Product(
+CREATE TABLE IF NOT EXISTS Product(
  p_barcode  INT(15,0) not NULL,
  p_supplierID  VARCHAR(25) not NULL,
  p_type   VARCHAR(25) not NULL,
  p_price    DECIMAL(15,0) not NULL
 );
 
-CREATE TABLE Customer(
+CREATE TABLE IF NOT EXISTS Customer(
  cu_customerID  VARCHAR(30) not NULL,
  cu_storeID  VARCHAR(30) not NULL,
  cu_barcode  INT(15,0) not NULL
 );
 
-CREATE TABLE Store(
+CREATE TABLE IF NOT EXISTS Store(
  st_storeID  VARCHAR(25) not NULL,
  st_cityKey  INT(15,0) not NULL,
  st_type   VARCHAR(25) not NULL
 );
 
 
-CREATE TABLE Country(
+CREATE TABLE IF NOT EXISTS Country(
  c_countryKey  INT(15,0) not NULL,
- c_name    VARCHAR(25) not NULL,
- c_shippingRate INT(15,0) not NULL
+ c_name    VARCHAR(25) not NULL
+ c_shippingRate  DECIMAL(10,0) not NULL
 );
 
-CREATE TABLE City(
+CREATE TABLE IF NOT EXISTS City(
  ci_cityKey   INT(15,0) not NULL,
  ci_countryKey   INT(15,0) not NULL,
  ci_name   VARCHAR(25) not NULL
 );
 
-CREATE TABLE StoreSupp(
+CREATE TABLE IF NOT EXISTS StoreSupp(
  stu_storeID VARCHAR(25) not NULL,
  stu_suppID  VARCHAR(25) not NULL
 );
 
-CREATE TABLE ProductCustomer(
+CREATE TABLE IF NOT EXISTS ProductCustomer(
  pc_custID      VARCHAR(30) not NULL,
  pc_barcode     INT(15,0) not NULL
 );
